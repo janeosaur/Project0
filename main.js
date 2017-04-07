5a9d5291aff1a92a79bac9b9ab10f967a33d28db
janeosaur <janewie@gmail.com>
2017-04-07 15:43:44 -0700

    added functions to hide images and replay

--- main.js
+++ main.js
@@ -10,26 +10,62 @@ $(document).on("ready", function(){
 
   var firstTime = true; // to ensure play button clickable only once
 
-  $('#play').on('click', showPuppyGif);
+  $('#play').on('click', startGame);
 
+  function startGame() {
+    setTimeout(showPuppyGif, 500);
+    showImages();
+    // setInterval(showImages, 5000); // try to make interval times random?
+
+  } // ends startGame function on play button click
+
+
+  // in randomizing loop...
+  // var images = 'images/'+imageSource[i]
+  // add class to images when appended to page, so that browser knows
+  // it's bad to click on puppy image?
+
+// idea: to have images stack on top of eachother: http://stackoverflow.com/questions/25393877/put-2-images-on-top-of-each-other
+
+  // defining functions
   function showPuppyGif() {
     if (firstTime) {
       firstTime = false;
-      var img = document.createElement("img");
-      img.src = "images/puppy.gif"
-      $('.images').append(img).addClass("gif").fadeOut(4500);
-      $('.messages').append('<p> Game is Loading... </p>').fadeOut(4000);
+      $('.images').append('<img id="gif" src="images/puppy.gif"/>');
+      $('.messages').append($('<p> Game is Loading... </p>', {id: 'loading'}));
     } else {
       console.log('press replay to play again')
     } // next: animate the ellipsis and have pop up image to instruct player to press replay
-  } // ends showPuppyGif function on play button click
+  } // end of showPuppyGif
 
-// after loading screen fades out.. have random (? or looped) zombie image appear -
-//  would this need to be linked with the play on click function??
-// on click, image disappears and new image loads after a few,random seconds
+    // holds gallery of images in array
+  var imageSource = ['zombie1.png', 'zombie2.png', 'zombie3.png', 'zombie4.png', 'zpuppy.jpg', 'puppy1.png', 'puppy2.png', 'puppy3.png', 'puppy4.png'];
+
+  function showImages() {
+
+    setTimeout(hidePuppy, 3000);
+    function hidePuppy() {
+      $('#puppy').children().fadeOut(2000, function() {
+        $('#puppy').empty();
+      }) //to hide puppy image>
+    }
+  } // ends showImages function
 
 
 
-
+  // replay function --
+  function replay() {
+    firstTime = true;
+  }
 
 }); // ends doc on ready
+
+
+// tried to make image object constructor to hold id and src but this didn't append to page...
+  // function ImageConstruct(idname, srcpath) {
+  //   this.id = idname,
+  //   this.src = srcpath
+  // }
+  // var loopedImage = imageSource[0] // try to loop...
+  // var zombie = new ImageConstruct('zombie', loopedImage)
+  // $('body').append($('<img>', zombie));
