6abb5db3dc69a5f27f8da2afc58bfc15e3502cbc
janeosaur <janewie@gmail.com>
2017-04-06 22:39:48 -0700

    added game is loading text and gif

--- main.js
+++ main.js
@@ -3,6 +3,25 @@ console.log("JS is linked!");
 
 $(document).on("ready", function(){
 
+// will need these variables if i want to record reaction time...
+  // var clickedTime;
+  // var createdTime;
+  // var reactionTime;
 
 
-});
+
+// on user hitting play, puppy gif shows up only once
+  $('#play').on('click', showPuppyGif);
+
+  function showPuppyGif() {
+    var firstTime = true;
+    if (firstTime) {
+      firstTime = false;
+      var img = document.createElement("img");
+      img.src = "images/puppy.gif"
+      $('.images').append(img).addClass("images");
+      $('.messages').append('<p> Game is Loading... </p>')
+    }
+  }
+
+}); // ends doc on ready
